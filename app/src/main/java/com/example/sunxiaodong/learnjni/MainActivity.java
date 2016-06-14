a966fa2601c9ee837913f1802a2a89b0e0d800b2
孙晓冬 <sunxiaodong@sunxiaodongdeMacBook-Pro.local>
2016-06-15 00:39:30 +0800

    Android JNI开发示例
    Android JNI开发示例

---
+++ app/src/main/java/com/example/sunxiaodong/learnjni/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sunxiaodong.learnjni;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("myjni");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText(getJniMessage());
+    }
+
+    public native String getJniMessage();
+
+}
